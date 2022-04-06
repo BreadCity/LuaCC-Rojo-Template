5d5c1f27fa09ac61ac21561e7ef57999c5fdcf8f
YieldingCoder <90570076+YieldingExploiter@users.noreply.github.com>
2022-04-06 14:51:44 +0200

    chore: Reflect Changes in https://github.com/BreadCity/LuaCC-Template/commit/101a8d8b635e3fcd4d453fd925eaa2f50507be69

--- bundle.ps1
+++ bundle.ps1
@@ -1,6 +1,6 @@
 Write-Output "" > nou.txt
 mkdir dist 2> nou.txt
 Set-Location src/StarterGUI/Scripts/Main
-node ../../../../bundle.js
+node ../../../../bundle/index.js
 Set-Location ../../../..
 Remove-Item nou.txt
